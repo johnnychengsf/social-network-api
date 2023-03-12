04cf24d1aed0488c1f4f9bea6dcccfadb4bc2fc2
Cheung Yee Cheng <johnny.cheng.sf@gmail.com>
2023-03-11 18:28:45 -0800

    fix thought  model issue

--- models/Thought.js
+++ models/Thought.js
@@ -1,42 +1,66 @@
 const { Schema, model } = require('mongoose');
-//const Reaction = require('./Reaction');
+const reactionSchema = require('./Reaction');
 
-// Schema to create Post model
 const thoughtSchema = new Schema(
   {
     thoughtText: {
       type: String,
       required: true,
-      trim: true,
+      minlength: 1,
       maxlength: 280,
     },
     createdAt: {
       type: Date,
       default: Date.now,
-      get: (createdAtVal) => dateFormat(createdAtVal),
+      get: (timestamp) => dateFormat(timestamp),
     },
     username: {
       type: String,
       required: true,
-      trim: true,
     },
-    
-    //reactions: [reactionSchema],
-    reactions: [Reaction],
+    reactions: [reactionSchema.schema],
+    /*
+    reactions: [
+      {
+        reactionId: {
+          type: Schema.Types.ObjectId,
+          default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+          type: String,
+          required: true,
+          maxlength: 280,
+        },
+        username: {
+          type: String,
+          required: true,
+        },
+        createdAt: {
+          type: Date,
+          default: Date.now,
+          get: (timestamp) => dateFormat(timestamp),
+        },
+      },
+    ],
+    */
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
   }
 );
 
-// Define a virtual property called reactionCount that retrieves the length of the reactions array field on query
-thoughtSchema.virtual('reactionCount').get(function() {
+thoughtSchema.virtual('reactionCount').get(function () {
   return this.reactions.length;
 });
 
-// Format timestamp on query
-const dateFormat = (createdAtVal) => {
-  return new Date(createdAtVal).toISOString();
-};
+function dateFormat(timestamp) {
+  return new Date(timestamp).toISOString();
+}
 
-// Initialize our Thought model
-const Thought = model('thought', thoughtSchema);
+const Thought = model('Thought', thoughtSchema);
 
 module.exports = Thought;
